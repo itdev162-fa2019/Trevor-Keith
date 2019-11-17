2a8d8a360b01e820f76a63a1e90b889f97121d6a
Trevor Keith <keithtw@gmatc.matc.edu>
2019-11-17 01:46:31 -0600

    Created Delete Post endpoint

--- API/Controllers/PostsController.cs
+++ API/Controllers/PostsController.cs
@@ -95,6 +95,33 @@ namespace API.Controllers
 
         }
 
+        /// <summary>
+        /// DELETE api/post/[id]
+        /// </summary>
+        /// <param name="id">Post id</param>
+        /// <returns>True, is successful</returns>
+        [HttpDelete("{id}")]
+        public ActionResult<bool> Delete(Guid id) {
+            var post = context.Posts.Find(id);
+
+            if(post == null) {
+                throw new Exception("Could not find post");
+
+            }
+
+            context.Remove(post);
+
+            var success = context.SaveChanges() > 0;
+
+            if(success) {
+                return true;
+
+            }
+
+            throw new Exception("Error deleting post");
+
+        }
+
     }
 
 }
